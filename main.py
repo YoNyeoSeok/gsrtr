37840a4149bb9a76561a6941fcb5c86b4ed5fdcc
yonyeoseok <yys8646@postech.ac.kr>
2022-01-31 17:20:11 +0900

    UPDATE lr_drop 30

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_args_parser():
     parser = argparse.ArgumentParser('Set grounded situation recognition transformer', add_help=False)
     parser.add_argument('--lr', default=1e-4, type=float)
     parser.add_argument('--lr_backbone', default=1e-5, type=float)
-    parser.add_argument('--lr_drop', default=100, type=int)
+    parser.add_argument('--lr_drop', default=30, type=int)
     parser.add_argument('--weight_decay', default=1e-4, type=float)
     parser.add_argument('--clip_max_norm', default=0.1, type=float,
                         help='gradient clipping max norm')
